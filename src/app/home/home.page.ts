4ecd2485f9240610152115d7a2e92d96498396b4
Jorge Martínez <jorgeme0996@gmail.com>
2019-05-20 18:36:28 -0500

    Se sube PushApp

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ApplicationRef } from '@angular/core';
 import { PushService } from '../services/push.service';
+import { OSNotificationPayload } from '@ionic-native/onesignal/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  
-  constructor(public pushService:PushService){
+export class HomePage implements OnInit {
+  mensajes: OSNotificationPayload[] = [];
+  constructor(public pushService:PushService, private aplicationRef: ApplicationRef){
 
+  }
+
+  ngOnInit(){
+    this.pushService.pushListener.subscribe(noti => {
+      this.mensajes.unshift(noti);
+      this.aplicationRef.tick();
+    })
+  }
+
+  async ionViewWillEnter(){
+    this.mensajes = await this.pushService.getMensajes()
   }
 
 }
--- src/app/services/push.service.ts
+++ src/app/services/push.service.ts
@@ -1,36 +1,71 @@
-import { Injectable } from '@angular/core';
-import { OneSignal } from '@ionic-native/onesignal/ngx';
+import { Injectable, EventEmitter } from '@angular/core';
+import { OneSignal, OSNotification, OSNotificationPayload } from '@ionic-native/onesignal/ngx';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PushService {
 
-  mensajes: any[] = [
-    {
-      title: 'Turilo de la push',
-      body: 'Este es el body de la push',
-      date: new Date()
-    }
-  ];
+  mensajes: OSNotificationPayload[];
 
-  constructor(private oneSignal: OneSignal) { }
+  pushListener = new EventEmitter<OSNotificationPayload>();
+
+  constructor(private oneSignal: OneSignal, private storage:Storage) { 
+    this.cargarMensajes();
+  }
+
+  async getMensajes(){
+    await this.cargarMensajes();
+    return [...this.mensajes];
+  }
 
   configuraciónInicial(){
     this.oneSignal.startInit('0af4b726-f3c3-4d99-8672-7f10dea936ce', '317555251089');
 
-    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+    this.oneSignal.inFocusDisplaying( this.oneSignal.OSInFocusDisplayOption.Notification );
 
     this.oneSignal.handleNotificationReceived().subscribe((noti) => {
     // do something when notification is received
       console.log('Notifricación recibida',noti);
+      this.notificacionRecibida( noti )
     });
 
-    this.oneSignal.handleNotificationOpened().subscribe((noti) => {
+    this.oneSignal.handleNotificationOpened().subscribe( async (noti) => {
       // do something when a notification is opened
       console.log('Notifricación abierta',noti);
+      await this.notificacionRecibida( noti.notification );
     });
 
     this.oneSignal.endInit();
+  }
+
+  async notificacionRecibida(noti:OSNotification){
+
+    await this.cargarMensajes();
+
+    const payload = noti.payload;
+
+    const existePush = this.mensajes.find( mensaje => mensaje.notificationID === payload.notificationID );
+
+    if( existePush) {
+      return
+    }
+
+    this.mensajes.unshift(payload);
+    this.pushListener.emit(payload);
+
+    await this.guardarMensajes();
+
+    
+  }
+
+  guardarMensajes(){
+    this.storage.set('mensajes', this.mensajes);
+  }
+
+  async cargarMensajes(){
+    this.mensajes = await this.storage.get('mensajes') || [];
+    return this.mensajes;
   }
 }
